c732ff9c84b50eaa75bebaa58dc8587b5283d17b
Andrew Garcia <garcia.gtr@gmail.com>
2024-08-09 23:59:36 -0500

    add octree bindings in voxel.cpp

--- VoxelForge/voxel.cpp
+++ VoxelForge/voxel.cpp
@@ -1,4 +1,5 @@
 #include "voxel.h"
+#include "octree.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -42,6 +43,7 @@ std::vector<std::pair<int, int>> VoxelGrid::toGraph(int xDim, int yDim, int zDim
 }
 
 PYBIND11_MODULE(voxelforge_cpp, m) {
+    // Existing bindings
     py::class_<Voxel>(m, "Voxel")
         .def(py::init<int, int, int>())
         .def_readwrite("x", &Voxel::x)
@@ -55,4 +57,21 @@ PYBIND11_MODULE(voxelforge_cpp, m) {
             return grid.voxels;
         })
         .def("toGraph", &VoxelGrid::toGraph);
+
+    // Octree bindings
+    py::class_<OctreeNode, std::shared_ptr<OctreeNode>>(m, "OctreeNode")
+        .def("is_leaf", &OctreeNode::IsLeaf);
+
+    py::class_<OctreeLeafNode, OctreeNode, std::shared_ptr<OctreeLeafNode>>(m, "OctreeLeafNode")
+        .def(py::init<const Eigen::Vector3d&>())
+        .def("get_point", &OctreeLeafNode::GetPoint);
+
+    py::class_<OctreeInternalNode, OctreeNode, std::shared_ptr<OctreeInternalNode>>(m, "OctreeInternalNode")
+        .def(py::init<>())
+        .def("get_child", &OctreeInternalNode::GetChild);
+
+    py::class_<Octree, std::shared_ptr<Octree>>(m, "Octree")
+        .def(py::init<const Eigen::Vector3d&, double, size_t>())
+        .def("insert_point", &Octree::InsertPoint)
+        .def("locate_leaf_node", &Octree::LocateLeafNode);
 }
